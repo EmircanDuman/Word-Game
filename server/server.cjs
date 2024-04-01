383218cd95e6935349fb2e4b72b725bfd9e46f65
Emircan Duman <emircandum10@gmail.com>
2024-04-01 13:11:06 +0300

    Update server.cjs

--- server/server.cjs
+++ server/server.cjs
@@ -31,7 +31,6 @@ const PORT = process.env.PORT || 3000;
 // Endpoint to check if user exists with provided name and password
 app.get("/user", async (req, res) => {
   const { name, password } = req.query;
-  console.log("new request recieved");
 
   try {
     const user = await User.findOne({ name, password }).exec();
@@ -48,7 +47,6 @@ app.get("/user", async (req, res) => {
 
 app.get("/user/checkname", async (req, res) => {
   const { name } = req.query;
-  console.log("New request received to check user by name");
 
   try {
     // Find a user with the provided name
@@ -105,6 +103,32 @@ app.post("/api/heartbeat", async (req, res) => {
     res.status(500).send("Internal server error");
   }
 });
+
+const checkAndUpdateStatus = async () => {
+  try {
+    console.log("Updating online status for all users...");
+    // Find users whose status is "Online" or "In-game"
+    const users = await User.find({ status: { $in: ["Online", "In-game"] } });
+
+    // Iterate over each user
+    users.forEach(async (user) => {
+      // Check if lastActive is more than 15 seconds old
+      if (user.lastActive.getTime() < Date.now() - 15000) {
+        // Update user status to "Offline"
+        await User.findOneAndUpdate(
+          { _id: user._id },
+          { $set: { status: "Offline" } }
+        );
+        console.log(`User ${user.name} status updated to "Offline"`);
+      }
+    });
+  } catch (error) {
+    console.error("Error checking and updating status:", error);
+  }
+};
+
+// Run the checkAndUpdateStatus function every 16 seconds
+setInterval(checkAndUpdateStatus, 16000);
 
 app.listen(PORT, () => {
   console.log(`Server is running on port ${PORT}`);
